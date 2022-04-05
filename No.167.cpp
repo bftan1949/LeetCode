949fe4466e97f5d396699c0d70c3ff1b789622c9
BaoFeng Tan <79528395+Castle-cn@users.noreply.github.com>
2022-04-05 21:53:06 +0800

    Create No.167.cpp

---
+++ No.167.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] > target)
+                right--;
+            else if (numbers[left] + numbers[right] < target)
+                left++;
+            else
+                return vector<int>{left + 1, right + 1};
+        }
+        return vector<int>{-1, -1};
+    }
+};
