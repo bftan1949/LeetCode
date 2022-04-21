2e7aca67977a810aeb89841eed8d0f21158a6426
BaoFeng Tan <79528395+Castle-cn@users.noreply.github.com>
2022-04-21 17:42:23 +0800

    Update 567. 字符串的排列.cpp

--- 滑动窗口/567. 字符串的排列.cpp
+++ 滑动窗口/567. 字符串的排列.cpp
@@ -1,25 +1,22 @@
 class Solution {
    public:
     bool checkInclusion(string s1, string s2) {
-        int n = s1.length(), m = s2.length();
-        if (n > m) {
-            return false;
-        }
-        vector<int> cnt(26);
-        for (int i = 0; i < n; ++i) {
-            --cnt[s1[i] - 'a'];
-        }
-        int left = 0;
-        for (int right = 0; right < m; ++right) {
-            int x = s2[right] - 'a';
-            ++cnt[x];
-            while (cnt[x] > 0) {
-                --cnt[s2[left] - 'a'];
-                ++left;
+        int size_1 = s1.size(), size_2 = s2.size();
+        int left = 0, right = 0, flag[26] = {0};
+
+        // 对s1每个字符进行统计
+        for (auto it = s1.begin(); it < s1.end(); it++)
+            flag[(*it) - 'a']++;
+
+        while (right < size_2) {
+            flag[s2[right] - 'a']--;
+            while (flag[s2[right] - 'a'] < 0) {
+                flag[s2[left] - 'a']++;
+                left++;
             }
-            if (right - left + 1 == n) {
+            if (right - left + 1 == size_1)
                 return true;
-            }
+            right++;
         }
         return false;
     }
